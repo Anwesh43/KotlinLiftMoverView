35f2dd28c503d669d441126b122c93b49db99f08
Anwesh43 <anweshthecool0@gmail.com>
2018-04-22 03:04:09 +0530

    adding LiftView in Activity and enabling fullScreen support

--- app/src/main/java/ui/anwesome/com/kotlinliftview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinliftview/MainActivity.kt
@@ -2,10 +2,19 @@ package ui.anwesome.com.kotlinliftview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import ui.anwesome.com.liftview.LiftView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LiftView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
