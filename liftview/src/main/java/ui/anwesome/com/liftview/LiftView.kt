3f2f5d2e204a250f07d1463a2f6e19324e432a7e
Anwesh43 <anweshthecool0@gmail.com>
2018-04-22 01:44:10 +0530

    adding LiftView class

--- liftview/src/main/java/ui/anwesome/com/liftview/LiftView.kt
+++ liftview/src/main/java/ui/anwesome/com/liftview/LiftView.kt
@@ -3,3 +3,26 @@ package ui.anwesome.com.liftview
 /**
  * Created by anweshmishra on 22/04/18.
  */
+
+import android.view.MotionEvent
+import android.view.View
+import android.graphics.*
+import android.content.Context
+
+class LiftView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
